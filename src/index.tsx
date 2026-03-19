42fa54f90f422a62308da01261095d0384b87948
TheKost_ <thekost2@yandex.ru>
2026-03-19 19:17:08 +0300

    add form logic add styles setting state add styles setting sync to style page

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { Article } from 'components/article';
+import { ArticleParamsForm } from 'components/article-params-form';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +15,30 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+const mapSettingsToCSSProperties = (
+	settings: ArticleStateType
+): CSSProperties =>
+	({
+		'--font-family': settings.fontFamilyOption.value,
+		'--font-size': settings.fontSizeOption.value,
+		'--font-color': settings.fontColor.value,
+		'--container-width': settings.contentWidth.value,
+		'--bg-color': settings.backgroundColor.value,
+	} as CSSProperties);
+
 const App = () => {
+	const [stylesSettings, setStylesSettings] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+			style={mapSettingsToCSSProperties(stylesSettings)}>
+			<ArticleParamsForm
+				settings={stylesSettings}
+				onApply={setStylesSettings}
+				onReset={() => setStylesSettings(defaultArticleState)}
+			/>
 			<Article />
 		</main>
 	);
